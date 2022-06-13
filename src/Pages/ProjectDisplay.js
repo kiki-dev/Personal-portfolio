14a6c5b9703ff97dd0b42ccbe1fe860460b6139c
arezki-kroun <krounarezki308@gmail.com>
2022-06-13 02:19:14 +0200

    Second version 2

--- src/Pages/ProjectDisplay.js
+++ src/Pages/ProjectDisplay.js
@@ -14,7 +14,7 @@ function ProjectDisplay() {
       <p>
         <b>Skills:</b> {project.skills}
       </p>
-      <GitHubIcon />
+     <a href="https://github.com/kiki-dev"><GitHubIcon /></a>
     </div>
   );
 }
